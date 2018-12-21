21162f69a5d0ddc71c318803d0c349096ca7f3d2
Ben Houston <BenjaminAHouston@gmail.com>
2018-12-20 23:30:18 -0500

    Wrote the schema for the notes and articles, also wrote the api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,65 @@
-// let db = require('../models')
+let db = require('../models');
+let axios = require('axios');
+let cheerio = require('cheerio');
 
 module.exports = function(app) {
-    
+    app.get('/api/article', (req, res) => {
+        db.Article.find({}).then((data) => {
+            res.json(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    app.get('/api/article/:id', (req, res) => {
+        db.Article.findById(req.params.id).then((data) => {
+            res.json(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    app.get('/api/save/:id', (req, res) => {
+        db.Article.findByIdAndUpdate(req.params.id, { $set: { saved: true } }).then((data) => {
+            res.json(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    app.get('/api/unsave/:id', (req, res) => {
+        db.Article.findByIdAndUpdate(req.params.id, { $set: { saved: false } }).then((data) => {
+            res.json(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    app.get('/api/saved', (req, res) => {
+        db.Article.find({ saved: true }).then((data) => {
+            res.json(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    app.get('/api/scrape', (req, res) => {
+        axios.get('https://apnews.com/').then((response) => {
+            let $ = cheerio.load(response.data);
+
+            $('div.FeedCard').each((i, element) => {
+                let article = {
+                    url: 'https://apnews.com/' + $(element).find('a.headline').attr('href'),
+                    headline: $(element).find('a.headline').text(),
+                    summary: $(element).find('div.content').find('p').text()
+                }
+                db.Article.create(article).then(dbArticle => {
+                    console.log(dbArticle);
+                }).catch(err => {
+                    console.log(err);
+                })
+            });
+            res.send('Scrape Complete');
+        });
+    });
 }
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 let express = require('express');
 let exphbs = require('express-handlebars');
 let mongoose = require('mongoose');
-let axios = require('axios');
-let cheerio = require('cheerio');
 
 let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 
-// let db = require('./models');
+let db = require('./models');
 
 const PORT = 3000;
 
 let app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
